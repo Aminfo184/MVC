7a8706c475730498c5bb24461de73a54958ddb15
Amin fo <aminfoprogramming@gmail.com>
2022-06-05 14:40:38 +0430

    Test view method (trait)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,12 +5,11 @@ namespace Application\Controllers;
 class Home extends Controller{
 
     public function index(){
-        echo "this is a index method";
+        $this->view('app.index');
     }
 
     public function create(){
         echo "this is a create method";
     }
-
 
 }
