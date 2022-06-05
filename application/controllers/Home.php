e18621fb09c92ee47b2931e32cbea62c251f11ba
Amin fo <aminfoprogramming@gmail.com>
2022-06-05 14:33:09 +0430

    Test routing system

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Controllers;
+
+class Home extends Controller{
+
+    public function index(){
+        echo "this is a index method";
+    }
+
+    public function create(){
+        echo "this is a create method";
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include('system/config.php');
+include(__DIR__.'/system/router/Routing.php');
+
+include("application/controllers/Controller.php");
+include("application/controllers/Home.php");
+include('system/bootstrap/boot.php');
