6cfc6a1be69a5d15e694c61ec0a718ee41a4e77a
Mohammad-Amin-Forati <aminfoprogramming@gmail.com>
2022-06-11 15:04:56 +0430

    add include method

--- system/traits/View.php
+++ system/traits/View.php
@@ -23,4 +23,17 @@ trait View {
         echo $path;
     }
 
+    protected function include($dir, $vars = null)
+    {
+
+        $dir = str_replace('.', '/', $dir);
+        if ($vars)
+            extract($vars);
+        $path = realpath(dirname(__FILE__) . "/../../application/view/" . $dir . ".php");
+        if (file_exists($path)) {
+            return require_once($path);
+        } else
+            echo "this view [" . $dir . "] not exist";
+    }
+
 }
